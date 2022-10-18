41f57471a6aba094585ee1cc00106a29de17dfa2
Phong Tran <540580+phongse@users.noreply.github.com>
2022-10-18 05:11:49 +0200

    Exit application if `DATABASE_URL` is not set

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import { exit } from "process";
 
 import playerRouter from "./routes/player.router";
+
+if (!process.env.DATABASE_URL) {
+  console.log("Define DATABASE_URL in .env");
+  exit(1);
+}
 
 const app = express();
 
@@ -10,7 +16,9 @@ app.use(express.json());
 
 app.use("/player", playerRouter);
 
-const server = app.listen(process.env.SERVER_PORT, () =>
+const port = process.env.SERVER_PORT || 3000;
+
+const server = app.listen(port, () =>
   console.log(`
-🚀 Server ready at: http://localhost:${process.env.SERVER_PORT}`)
+🚀 Server ready at: http://localhost:${port}`)
 );
